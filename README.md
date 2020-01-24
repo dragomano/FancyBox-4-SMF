595b8ec46c4647fa214294f1378ce53aef5a5f77
Bugo <bugo@dragomano.ru>
2020-01-24 13:52:58 +0500

    Upload to GitHub, v0.7

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # FancyBox-4-SMF
 Просмотр изображений в сообщениях, с эффектами анимации https://dragomano.ru/mods/fancybox-4-smf
+
+# FancyBox-4-SMF
+![SMF 2.1](https://img.shields.io/badge/SMF-2.1-blue.svg?style=flat&color=ed6033)
+![License](https://img.shields.io/github/license/dragomano/fancybox-4-smf)
+
+* **Author:** Bugo [dragomano.ru](https://dragomano.ru/mods/fancybox-4-smf)
+* **License:** [GPL-3.0](https://github.com/dragomano/FancyBox-4-SMF/blob/master/LICENSE)
+* **Compatible with:** SMF 2.1 RC2+
+* **Tested on:** PHP 7.3.13
+* **Hooks only:** Yes
+* **Languages:** English, Russian
+
+## Description
+[fancyBox](https://fancyapps.com/fancybox/3/) is a tool that offers a nice and elegant way to add zooming functionality for images on your forum. It is built at the top of the popular JavaScript framework jQuery and is both easy to implement and a snap to customize.
+
+## Описание
+Использование для просмотра изображений в сообщениях лёгкой и очень красивой jQuery-библиотеки [fancyBox](https://fancyapps.com/fancybox/3/).
---
+++ Themes/default/languages/FancyBox/.english.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * .english language file
+ *
+ * @package FancyBox 4 SMF
+ * @author Bugo https://dragomano.ru/mods/fancybox-4-smf
+ */
+
+$txt['fancybox_settings'] = 'FancyBox 4 SMF';
+$txt['fancybox_desc'] = 'This modification adds nice fancybox effects to in-line and attached images.';
+$txt['fancybox_image_preload'] = 'Wait for images to load before displaying';
+$txt['fancybox_animation_effect'] = 'Open/close animation effect';
+$txt['fancybox_animation_effect_set'] = array(
+	false => $txt['no'], 'zoom' => 'Zoom images from/to thumbnail', 'fade' => 'Fade appearance/disappearance', 'zoom-in-out' => 'Elastic zoom in/out'
+);
+$txt['fancybox_animation_speed'] = 'Duration in ms for open/close animation';
+$txt['fancybox_transition_effect'] = 'Transition effect between slides';
+$txt['fancybox_transition_effect_set'] = array(
+	false => $txt['no'], 'fade' => 'Fade transition', 'slide' => 'slide', 'circular' => 'circular', 'tube' => 'tube', 'zoom-in-out' => 'zoom-in-out', 'rotate' => 'rotate');
+$txt['fancybox_transition_speed'] = 'Duration in ms for transition animation';
+$txt['fancybox_autoplay'] = 'Autoplay when any image is opening';
+$txt['fancybox_slideshow_speed'] = 'Delay between slides, in milliseconds';
+$txt['fancybox_show_link'] = 'Show download link under slides';
+$txt['fancybox_thumbnails'] = 'Show thumbnails below slides';
+$txt['fancybox_prepare'] = 'Processing of images that inserted with <strong>img</strong> tag';
+$txt['fancybox_traffic'] = 'Traffic saving <div class="smalltext">If enabled, unregistered users will see a special small image with running little man instead of original images. When clicked on it, they will see an original image.</div>';
+
+$txt['fancy_click'] = 'Click to load original image';
+$txt['fancy_download_link'] = 'Download image';
+$txt['fancy_text_error'] = 'The requested content cannot be loaded.<br />Please try again later.';
+$txt['fancy_button_next'] = 'Next';
+$txt['fancy_button_prev'] = 'Previous';
+$txt['fancy_slideshow_start'] = 'Start slideshow';
+$txt['fancy_slideshow_pause'] = 'Pause slideshow';
+$txt['fancy_full_screen'] = 'Full screen';
+$txt['fancy_thumbnails'] = 'Thumbnails';
+$txt['fancy_button_download'] = 'Download';
+$txt['fancy_button_share'] = 'Share';
+$txt['fancy_button_zoom'] = 'Zoom';
