8eed9ca2b16c59011008cb3128e8262624d289a6
Bugo <cheetah@bk.ru>
2023-04-17 14:47:03 +0500

    Update text strings

--- Themes/default/languages/FancyBox/.english.php
+++ Themes/default/languages/FancyBox/.english.php
@@ -9,8 +9,6 @@
 
 $txt['fancybox_settings'] = 'FancyBox 4 SMF';
 $txt['fancybox_desc'] = 'This modification adds nice fancybox effects to in-line and attached images.';
-$txt['fancybox_show_download_link'] = 'Show download button';
-$txt['fancybox_thumbnails'] = 'Show thumbnails below slides';
 $txt['fancybox_prepare_img'] = 'Processing of images that inserted with the <strong>[img]</strong> tag';
 $txt['fancybox_show_thumb_for_img'] = 'Display thumbnails of images inserted using the <strong>[img]</strong> tag';
 $txt['fancybox_save_url_img'] = 'If the images are inside the links, leave the links untouched';
@@ -18,6 +16,9 @@ $txt['fancybox_show_thumb_for_img_subtext'] = 'You can specify the width of imag
 $txt['fancybox_prepare_attach'] = 'Processing of images that inserted with the <strong>[attach]</strong> tag';
 $txt['fancybox_show_thumb_for_attach'] = 'Display thumbnails of images inserted using the <strong>[attach]</strong> tag';
 $txt['fancybox_show_thumb_for_attach_subtext'] = 'Disable to display images unchanged.';
+$txt['fancybox_prepare_attachments'] = 'Processing of attached images';
+$txt['fancybox_show_download_link'] = 'Show download button';
+$txt['fancybox_thumbnails'] = 'Show thumbnails below slides';
 $txt['fancybox_traffic'] = 'Traffic saving <div class="smalltext">If enabled, unregistered users will see a special small image with running little man instead of original images. When clicked on it, they will see an original image.</div>';
 
 $txt['fancy_click'] = 'Click to load original image';
